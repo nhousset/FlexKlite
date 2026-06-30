f492691dab8ba1c095f2dfe9c238592379136add
Nicolas Housset <nicolas.housset@gmail.com>
2026-06-30 20:01:49 +0200

    refactor(ui): extraction de l'onglet Kanban vers une vue partielle dédiée

---
+++ src/kanban.php
@@ -0,0 +1,20 @@
+<div id="tab-kanban" class="tab-content active">
+    <div class="board">
+        <div class="column" id="todo">
+            <h3>À Faire</h3>
+            <div class="list" data-status="todo"></div>
+        </div>
+        <div class="column" id="in_progress">
+            <h3>En Cours</h3>
+            <div class="list" data-status="in_progress"></div>
+        </div>
+        <div class="column" id="blocked">
+            <h3>En attente / Bloqué</h3>
+            <div class="list" data-status="blocked"></div>
+        </div>
+        <div class="column" id="done">
+            <h3>Terminé</h3>
+            <div class="list" data-status="done"></div>
+        </div>
+    </div>
+</div>
