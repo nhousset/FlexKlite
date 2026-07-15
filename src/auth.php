80fb3bb26a90c9991dc692a4b5cac9189a059a23
Nicolas Housset <nicolas.housset@gmail.com>
2026-07-15 19:34:05 +0200

    feat(auth): add specific access tier for read-only sessions

--- src/auth.php
+++ src/auth.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
-// Le statut de connexion est vrai si la variable de session est définie
+
+// Le statut de connexion administrateur (Droits d'écriture)
 $is_logged_in = isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+
+// Le statut d'accès en lecture (Autorisé si admin OU si connecté en visiteur)
+$has_read_access = $is_logged_in || (isset($_SESSION['readonly_logged_in']) && $_SESSION['readonly_logged_in'] === true);
 ?>
