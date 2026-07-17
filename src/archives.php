46512f87826d9d59948e92eed4011555c54aad2e
Nicolas Housset <nicolas.housset@gmail.com>
2026-07-17 19:11:51 +0200

    fix header

--- src/archives.php
+++ src/archives.php
@@ -3,13 +3,27 @@
         <table class="data-table">
             <thead>
                 <tr>
-                    <th class="sortable-header" onclick="handleSort('projet')">Projet <span class="sort-icon" id="sort-icon-projet"></span></th>
-                    <th class="sortable-header" onclick="handleSort('titre')">Titre <span class="sort-icon" id="sort-icon-titre"></span></th>
-                    <th class="sortable-header" onclick="handleSort('statut')">Ancien Statut <span class="sort-icon" id="sort-icon-statut"></span></th>
-                    <th class="sortable-header" onclick="handleSort('prio')">Priorité <span class="sort-icon" id="sort-icon-prio"></span></th>
-                    <th class="sortable-header" onclick="handleSort('acteur')">Acteur <span class="sort-icon" id="sort-icon-acteur"></span></th>
-                    <th class="sortable-header" onclick="handleSort('maj')">Date M.A.J <span class="sort-icon" id="sort-icon-maj"></span></th>
-                    <th>Derniers Échanges</th>
+                    <th style="width: 12%;">
+                        <div class="sortable-header" onclick="sortTable('projet', this)">Projet <span class="sort-icon"></span></div>
+                    </th>
+                    <th style="width: 20%;">
+                        <div class="sortable-header" style="margin-top: 15px;" onclick="sortTable('titre', this)">Titre <span class="sort-icon"></span></div>
+                    </th>
+                    <th style="width: 10%;">
+                        <div class="sortable-header" onclick="sortTable('statut', this)">Ancien Statut <span class="sort-icon"></span></div>
+                    </th>
+                    <th style="width: 8%;">
+                        <div class="sortable-header" onclick="sortTable('prio', this)">Priorité <span class="sort-icon"></span></div>
+                    </th>
+                    <th style="width: 12%;">
+                        <div class="sortable-header" onclick="sortTable('acteur', this)">Acteur <span class="sort-icon"></span></div>
+                    </th>
+                    <th style="width: 8%;">
+                        <div class="sortable-header" style="margin-top: 15px;" onclick="sortTable('maj', this)">Date M.A.J <span class="sort-icon"></span></div>
+                    </th>
+                    <th style="width: 30%;">
+                        <div style="margin-top: 15px;">Derniers Échanges</div>
+                    </th>
                 </tr>
             </thead>
             <tbody id="archive-table-body">
